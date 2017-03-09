8b086c01cb8e92b702275ee1b445e87fd9e3bb59
Saša Barišić <cartman300@net.hr>
2017-03-09 07:26:06 +0100

    Start of the .NET binding

--- Gravity.NET/Native.cs
+++ Gravity.NET/Native.cs
@@ -1,10 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GravityNET {
+	[UnmanagedFunctionPointer(Native.CConv, CharSet = Native.CSet)]
+	public delegate void GravityErrorCallback(ErrorType EType, string Description, ErrorDesc EDesc, IntPtr Userdata);
+
+	[UnmanagedFunctionPointer(Native.CConv, CharSet = Native.CSet)]
+	public delegate void GravityLogCallback(string Msg, IntPtr Userdata);
+
+	[UnmanagedFunctionPointer(Native.CConv, CharSet = Native.CSet)]
+	public delegate string GravityLoadFileCallback(string File, ref IntPtr Size, ref uint FileID, IntPtr Userdata);
+
+	public enum ErrorType {
+		ERROR_NONE = 0,
+		ERROR_SYNTAX,
+		ERROR_SEMANTIC,
+		ERROR_RUNTIME,
+		ERROR_IO,
+		WARNING
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct ErrorDesc {
+		public int Code;
+		public int LineNo;
+		public int ColNo;
+		public int FileID;
+		public int Offset;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct GravityDelegate {
+		public IntPtr Userdata;
+
+		public GravityLogCallback LogCallback;
+		public GravityErrorCallback ErrorCallback;
+		public IntPtr UnitTestCallback;
+		public IntPtr ParserCallback;
+		public IntPtr PreCodeCallback;
+		public GravityLoadFileCallback LoadFileCallback;
+		public IntPtr FilenameCallback;
+
+		public IntPtr BridgeInitInstance;
+		public IntPtr BridgeSetValue;
+		public IntPtr BridgeGetValue;
+		public IntPtr BridgeSetUndef;
+		public IntPtr BridgeGetUndef;
+		public IntPtr BridgeExecute;
+		public IntPtr BridgeSize;
+		public IntPtr BridgeFree;
+	}
+
+	// GravityVM and GravityCompiler are opaque, don't need Ptr suffix
+	[StructLayout(LayoutKind.Explicit, Pack = 1)]
+	public struct GravityVM {
+		[FieldOffset(0)]
+		public IntPtr Pointer;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct GravityCompiler {
+		public IntPtr Pointer;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct GravityClosurePtr {
+		public IntPtr Pointer;
+
+		public static implicit operator bool(GravityClosurePtr C) {
+			return C.Pointer != IntPtr.Zero;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct GravityClass {
+		public IntPtr IsA;
+	}
+
+	[StructLayout(LayoutKind.Explicit)]
+	public unsafe struct GravityValue {
+		const int Offset = 8;
+
+		[FieldOffset(0)]
+		public GravityClass* IsA;
+
+		[FieldOffset(Offset)]
+		public IntPtr Ptr;
+		[FieldOffset(Offset)]
+		public int N;
+		[FieldOffset(Offset)]
+		public double F;
+	}
+
 	public class Native {
+		internal const string DllName = "GravityLang";
+		internal const CallingConvention CConv = CallingConvention.Cdecl;
+		internal const CharSet CSet = CharSet.Ansi;
+
+		// VM
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern GravityVM gravity_vm_new(GravityDelegate D);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern bool gravity_vm_run(GravityVM VM, GravityClosurePtr Closure);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern GravityValue gravity_vm_result(GravityVM VM);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern double gravity_vm_time(GravityVM VM);
+
+		// Compiler
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern GravityCompiler gravity_compiler_create(GravityDelegate D);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern GravityClosurePtr gravity_compiler_run(GravityCompiler Compiler, string Src, IntPtr Len, uint FileID, bool IsStatic);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern void gravity_compiler_transfer(GravityCompiler Compiler, GravityVM VM);
+
+		// Shared
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern void gravity_value_dump(GravityValue V, StringBuilder Buffer, ushort Len);
 	}
 }
--- TestThingie/Program.cs
+++ TestThingie/Program.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using GravityNET;
+using System.IO;
 
 namespace TestThingie {
 	class Program {
+		static void OnError(ErrorType EType, string Desc, ErrorDesc EDesc, IntPtr Userdata) {
+			if (EType == ErrorType.ERROR_RUNTIME)
+				Console.Write("RUNTIME ERROR: ");
+			else
+				Console.Write("{0} on {1} ({2}, {3}): ", EType, EDesc.FileID, EDesc.LineNo, EDesc.ColNo);
+
+			Console.WriteLine(Desc);
+		}
+
 		static void Main(string[] args) {
 			Console.Title = "Gravity.NET Test";
+
+			GravityDelegate D = new GravityDelegate();
+			D.ErrorCallback = OnError;
+			GravityVM VM = Native.gravity_vm_new(D);
+
+			GravityCompiler Compiler = Native.gravity_compiler_create(D);
+
+			string Src = File.ReadAllText("test.gravity");
+			Console.WriteLine("{0}\n", Src);
+			GravityClosurePtr ClosureThingie = Native.gravity_compiler_run(Compiler, Src, (IntPtr)Src.Length, 0, true);
+			if (!ClosureThingie)
+				throw new Exception("Wat, could not compile?");
+
+			Native.gravity_compiler_transfer(Compiler, VM);
+			if (Native.gravity_vm_run(VM, ClosureThingie)) {
+				GravityValue V = Native.gravity_vm_result(VM);
+
+				StringBuilder Res = new StringBuilder(1024);
+				Native.gravity_value_dump(V, Res, (ushort)Res.MaxCapacity);
+				Console.WriteLine("RESULT: {0} (in {1} ms)", Res.ToString(), Native.gravity_vm_time(VM));
+			}
 
 			Console.WriteLine("Done!");
 			Console.ReadLine();
